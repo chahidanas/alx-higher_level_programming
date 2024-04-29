032c3e17317d280cb53976d5f55f4c9909fbb61a
Chahid Anas <chahidanas27@gmail.com>
2024-04-29 14:14:58 -0700

    commit

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
+
+1-multi_languages.js
+
+#!/usr/bin/node
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
