50b3a10bc938de8dac455acf2a10cb313d1f9f9f
chahid anas <chahidanas27@gmail.com>
2024-09-10 19:18:45 -0700

    commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if (character != 101 and character != 113):
-        print("{:c}".format(character), end='')
+for i in range(97, 123):
+    if i in [101,113];
+        continue
+     print("{}".format(chr(i)), end='')
