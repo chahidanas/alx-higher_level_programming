bb87737bf30ebc1da04c70b3cbf42afb5f5ae673
chahid anas <chahidanas27@gmail.com>
2024-09-18 21:18:50 -0700

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    number = 0
-    if type(roman_string) is not str or roman_string is None:
-        return (0)
+    if not isinstance(roman_string, str):
+        return 0
+    
+    # Roman numerals map
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    
+    # Invalid cases: empty string or invalid characters
+    if not roman_string:
+        return 0
+    
+    numbers = [data.get(char, 0) for char in roman_string]
+    
+    # Return 0 if any invalid Roman numeral character is found
+    if 0 in numbers:
+        return 0
+    
+    # Conversion logic
+    rep = 0
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i + 1]:
+            rep += numbers[i]
+        else:
+            rep -= numbers[i]
+    rep += numbers[-1]
+    
+    return rep
 
-    for i, char in enumerate(roman_string):
-        temp = roman[char]
-        try:
-            if temp < roman[roman_string[i + 1]]:
-                temp = temp * -1
-            except IndexError:
-                pass
-            number = number + temp
-        return number
